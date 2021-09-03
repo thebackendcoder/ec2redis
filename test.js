5afa6755805283eac2ada63d895a81fd95fd8577
Kashif Kalam <Kashif.Kalam@viacom18.com>
2021-09-03 20:02:37 +0530

    cjjec

---
+++ test.js
@@ -0,0 +1,15 @@
+const Redis = require('ioredis');
+
+const redis = new Redis({
+    host: "myfirstredis.89owxy.0001.aps1.cache.amazonaws.com",
+    port: 6379
+})
+
+async function fun(){
+    const value = await redis.set('key1', 'value');
+    console.log(value);
+    redis.quit();
+}
+
+fun();
+
